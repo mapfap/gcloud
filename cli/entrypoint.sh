5c46d82200059a1f2deec4919b742411a80b04c6
Sarun Wongtanakarn <2062638+mapfap@users.noreply.github.com>
2019-10-01 23:49:27 +0700

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
-sh -c "gcloud auth activate-service-account --key-file=$HOME/gcloud.json $*"
+sh -c "gcloud auth activate-service-account --key-file=$HOME/gcloud.json"
 sh -c "gcloud $*"
